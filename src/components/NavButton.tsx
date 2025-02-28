269da05d6102c1b88415bfe618f3de53c2fca0f5
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-28 19:25:25 +0500

    feat: button component

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,2 +1,9 @@
 import { LucideIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
+
+type Props = {
+    icon: LucideIcon,
+    label: string,
+    href?: string,
+}
